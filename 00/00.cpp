dc3e0b8d07ef44543122eb95273b50b806deb22a
KushagrJ <kushagr.j@gmail.com>
2022-02-20 21:43:22 +0530

    Update the Repository

---
+++ 00/00.cpp
@@ -0,0 +1,24 @@
+// C++20 Standard
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+    cout << "Hello, World!\n";
+
+    return 0;
+
+}
+
+
+
+
+
+/* Trivia - 00.cpp
+
+ *
+
+ * End of Trivia */
